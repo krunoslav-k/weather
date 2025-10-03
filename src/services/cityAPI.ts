2b7287bfa1ba0f5a099002b9e9d3c2e69e52cff3
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2025-10-03 16:12:22 +0200

    chore: move API keys to .env

--- src/services/cityAPI.ts
+++ src/services/cityAPI.ts
@@ -3,7 +3,7 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "765a9f95f7msh2a3daa2d4ac8831p17abadjsnc4b9cf9780f2",
+    "x-rapidapi-key": import.meta.env.VITE_GEO_API_KEY,
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
   },
 };
